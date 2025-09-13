82ad37119903dd41bded68520cbed53852f06492
ARIDHI-H <haridhi@upstream.com>
2025-09-13 20:53:46 +0200

    chore:force springboot classpath resources

--- src/test/java/com/lacoste/uat/CucumberSpringConfiguration.java
+++ src/test/java/com/lacoste/uat/CucumberSpringConfiguration.java
@@ -4,8 +4,10 @@ import io.cucumber.spring.CucumberContextConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 
 @CucumberContextConfiguration
 @SpringBootTest(classes = UspE2EApplication.class, webEnvironment = WebEnvironment.NONE)
+@TestPropertySource("classpath:application.yaml")
 public class CucumberSpringConfiguration {
 }
