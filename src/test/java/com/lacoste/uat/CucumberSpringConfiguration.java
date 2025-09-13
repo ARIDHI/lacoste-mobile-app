6868d6436d028d102ae1a253314695cc2cd11652
ARIDHI-H <haridhi@upstream.com>
2025-09-13 21:11:35 +0200

    chore:add application yaml modif

--- src/test/java/com/lacoste/uat/CucumberSpringConfiguration.java
+++ src/test/java/com/lacoste/uat/CucumberSpringConfiguration.java
@@ -3,8 +3,6 @@ package com.lacoste.uat;
 import io.cucumber.spring.CucumberContextConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestPropertySource;
 
 @CucumberContextConfiguration
 @SpringBootTest(classes = UspE2EApplication.class, webEnvironment = WebEnvironment.NONE)
